4f3e15e5815f4c8541341ab0af4cbae62fc0f596
Michael2004-ukpeh <marcelukpeh@gmail.com>
2022-04-11 11:51:07 +0100

    Connect to phantom

--- app/src/components/RenderNotConnectedContainer.js
+++ app/src/components/RenderNotConnectedContainer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const RenderNotConnectedContainer = ({connectWallet}) => {
     return (
         <button className="cta-button connect-wallet-button" onClick={connectWallet}>
-           Connect to Wallet
+           Connect to Phantom Solana Wallet
         </button>
 
     )
